88a99a7d6f1cf7cbb1a6c1f08636bfb20ff2dc3b
felipe.santos <felipe.santos@codata.pb.gov.br>
2024-05-06 14:54:25 -0300

    valida se parametro 2 maior que parametro 1, exibe tudo num for, lança exceção

--- src/domain/Contador.java
+++ src/domain/Contador.java
@@ -16,15 +16,24 @@ public class Contador {
 			//chamando o método contendo a lógica de contagem
 			contar(parametroUm, parametroDois);
 		
-		}catch (ParametrosInvalidosException exception) {
+		} catch (ParametrosInvalidosException exception) {
 			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
-		}
+			System.out.println(exception.getMessage());
+		} finally {
+            terminal.close();
+        }
 		
 	}
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-		
+		if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro.");
+        }
+			
 		int contagem = parametroDois - parametroUm;
 		//realizar o for para imprimir os números com base na variável contagem
+		for (int i = 1; i <= contagem; i++) {
+            System.out.println(i);
+        }
 	}
 }
--- src/exception/ParametrosInvalidosException.java
+++ src/exception/ParametrosInvalidosException.java
@@ -1,5 +1,7 @@
 package exception;
 
 public class ParametrosInvalidosException extends Exception{
-    
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }    
 }
